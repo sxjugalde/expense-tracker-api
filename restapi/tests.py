73d94527e63df6bed42b07c685809901c38cf774
sxjugalde <joseph.ugalde@systems-x.com>
2022-11-13 06:48:35 -0600

    Installed djangorestframework and updated installed apps in settings

--- restapi/tests.py
+++ restapi/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+# from django.test import TestCase
+from unittest import TestCase
+
 
 # Create your tests here.
+def two_integers_sum(a, b):
+    return 0
+
+
+class TestSum(TestCase):
+    def test_sum(self):
+        self.assertEqual(two_integers_sum(1, 2), 3)
+
+
+if __name__ == "__main__":
+    unittest.main()
